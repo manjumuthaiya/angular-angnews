d686b7fa21a0ae30d3c920bd0717a791472a210b
Manju Muthaiya <manju.muthaiya@gmail.com>
2014-05-25 22:09:40 +0530

    Angular completed upto chapter 7

---
+++ app/scripts/controllers/postview.js
@@ -0,0 +1,5 @@
+'user strict';
+
+app.controller('PostViewCtrl', function($scope, $routeParams, Post) {
+	$scope.post = Post.find($routeParams.postId);
+});
---
+++ app/scripts/services/post.js
@@ -0,0 +1,33 @@
+'use strict';
+
+app.factory('Post', function($firebase, FIREBASE_URL, User) {
+	var ref = new Firebase(FIREBASE_URL + 'posts');
+	var posts = $firebase(ref);
+
+	var Post = {
+		all: posts,
+		create: function(post) {
+			console.log(User.signedIn());
+			if(User.signedIn()) {
+				var user = User.getCurrent();
+				post.owner = user.username;
+				return posts.$add(post).then(function (ref) {
+			      var postId = ref.name();
+			 
+			      user.$child('posts').$child(postId).$set(postId);
+			 
+			      return postId;
+			    });
+			}
+			
+		},
+		find: function(postId) {
+			return posts.$child(postId);
+		},
+		delete: function(postId) {
+			return posts.$remove(postId);
+		}
+	};
+
+	return Post;
+});
